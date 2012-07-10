cc1a1f984f014a18bed164fee252cb3fc2008231
Paweł Kłosiewicz <pawelkl@gmail.com>
2012-07-10 11:21:00 +0200

    repaired

--- AccessDb.rb
+++ AccessDb.rb
@@ -1,6 +1,7 @@
 # require 'rubygems'
 require 'mongo'
 require 'json'
+# require 'mash'
 # require 'active_support'
 
 class AccessDb
@@ -13,49 +14,59 @@ class AccessDb
   end
 
   def upsert_by_id id, json
-    json = JSON.parse! json
+    # json = JSON.parse! json
     @coll.update({ :_id => id }, json, :upsert => true)
   end
 
   def upsert_by_meta json
-    json = JSON.parse! json
-    @coll.update({ :hash_md5 => json[:hash_md5] }, json, :upsert => true)
+    # json = JSON.parse! json
+    @coll.update({ "hash_md5" => json["hash_md5"] }, json, :upsert => true)
   end
 
   def remove json
-    json = JSON.parse! json
+    # json = JSON.parse! json
     if json[:_id].nil?
-      then @coll.remove({ :hash_md5 => json[:hash_md5]})
+      then @coll.remove({ "hash_md5" => json[:hash_md5]})
     else
       @coll.remove({:_id => json[:id]})
     end
   end
 
   def add_info json, param, value
-    json = JSON.parse! json
+    # json = JSON.parse! json
     if json[:_id].nil?
-      then @coll.update({ :hash_md5 => json[:hash_md5] }, '$set' => { param => value })
+      then @coll.update(
+        { "hash_md5" => json[:hash_md5] },
+        '$set' => { param => value }
+        )
     else
       @coll.update({ :_id => json[:id] }, '$set' => { param => value })
     end
   end
 
   def remove_info json, param
-    json = JSON.parse! json
+    # json = JSON.parse! json
     if json[:_id].nil?
-      then @coll.update({ :hash_md5 => json[:hash_md5] }, '$set' => { param => nil })
+      then @coll.update(
+        { "hash_md5" => json[:hash_md5] },
+        '$set' => { param => nil }
+        )
     else
       @coll.update({ :_id => json[:id] }, '$set' => { param => nil})
     end
   end
 
   def find json
-    json = JSON.parse! json
-    @coll.find_one({:hash_md5 => json[:hash_md5]},{:fields => {:_id=>0}})
+    # json = JSON.parse! json
+    @coll.find_one(
+      {"hash_md5" => json[:hash_md5]},
+      {:fields => {:_id=>0}}
+      )
+    # find.each_with_object({}){|(k,v), h| h[k.to_sym] = v}
   end
 
   def update query, update
-    json = JSON.parse! json
+    # json = JSON.parse! json
     @coll.find_and_modify(:query => query, :update => update)
   end
 end
@@ -72,26 +83,15 @@ if __FILE__ == $0
     end
 
     def teardown
-      @coll.remove({:hash_md5 => [:sara,:ania]})
+      @coll.remove({"hash_md5" => ["beata","ania","some_hash"]})
     end
 
-    def test_read
-      json = {:hash_md5 => :sara}
-      json = JSON.generate json
-      @coll.upsert_by_meta json
-      # puts id
-      find = @coll.find json
-      # assert_equal(json[:hash_md5], find.except!("_id")["hash_md5"])
-      assert_equal json[:hash_md5], find["hash_md5"], "json file didn't match input file"
-      #find.except!("_id")["hash_md5"])
-      # assert_equal(82,id)
-    end
-
-    def test_read_with_hash
-      json = {:hash_md5 => :lolcats}
+    def test_add
+      json = {:hash_md5 => "some_hash"}
       @coll.upsert_by_meta json
       find = @coll.find json
       # json[:_id] = id
+      find = find.each_with_object({}){|(k,v), h| h[k.to_sym] = v}
       assert_equal json, find, "retrieved json file isn't exactly the same as input json"
     end
 
@@ -99,7 +99,7 @@ if __FILE__ == $0
       json = {:hash_md5 => :ania}
       @coll.upsert_by_meta json
       @coll.add_info(json, :stan, :zajeta)
-      find = @coll.find(json)
+      find = @coll.find json
       assert_equal(json[:stan], find[:stan])
       @coll.remove_info(json, :stan)
       assert_equal(json[:stan], nil)
